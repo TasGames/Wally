17da7b26021de1f6b50d7836c824eb781ba3f2f1
Thomas Simon <thomas.simon@btopenworld.com>
2018-10-08 16:08:19 +0100

    Completed header and added to cpp

--- EscapeFromWally/Source/EscapeFromWally/PlayerCharacter.cpp
+++ EscapeFromWally/Source/EscapeFromWally/PlayerCharacter.cpp
@@ -1,14 +1,31 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Thomas Simon
 
 #include "PlayerCharacter.h"
-
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/InputSettings.h"
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	//Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
 
+	//Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); 
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	//Create a mesh component that will be used when being viewed from a 1st person view
+	MeshP = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	MeshP->SetOnlyOwnerSee(true);
+	MeshP->SetupAttachment(FirstPersonCameraComponent);
+	MeshP->bCastDynamicShadow = false;
+	MeshP->CastShadow = false;
+	MeshP->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	MeshP->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
 }
 
 // Called when the game starts or when spawned
@@ -18,17 +35,35 @@ void APlayerCharacter::BeginPlay()
 	
 }
 
-// Called every frame
-void APlayerCharacter::Tick(float DeltaTime)
+void APlayerCharacter::MoveVer(float Val)
 {
-	Super::Tick(DeltaTime);
+	if(Val != 0.0f)
+		AddMovementInput(GetActorForwardVector(), Val);
+}
 
+void APlayerCharacter::MoveHor(float Val)
+{
+	if (Val != 0.0f)
+		AddMovementInput(GetActorRightVector(), Val);
 }
 
 // Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	//Set up gameplay key bindings
+	check(PlayerInputComponent);
+
+	//Jump
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	//Move
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveVer);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveHor);
+
+	//Rotate
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 
 }
 
--- EscapeFromWally/Source/EscapeFromWally/PlayerCharacter.h
+++ EscapeFromWally/Source/EscapeFromWally/PlayerCharacter.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Thomas Simon
 
 #pragma once
 
@@ -17,7 +17,7 @@ class ESCAPEFROMWALLY_API APlayerCharacter : public ACharacter
 
 	//Players Arms
 	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
-	class USkeletalMeshComponent* Mesh1P;
+	class USkeletalMeshComponent* MeshP;
 
 	//Torch Mesh
 	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
@@ -31,10 +31,27 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+public:	
+	//How fast player turns horizontally
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseTurnRate;
+
+	//How fast the player looks vertically
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseLookUpRate;
+
+protected:
+
+	void MoveVer(float Val);
+	void MoveHor(float Val);
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-public:	
-
+public:
+	//Returns MeshP subobject
+	FORCEINLINE class USkeletalMeshComponent* GetMesh1P() const { return MeshP; }
+	//Returns FirstPersonCameraComponent subobject 
+	FORCEINLINE class UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
 	
 };
